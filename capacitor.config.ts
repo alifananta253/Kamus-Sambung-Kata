499efe06514dc7b6c90bac1f70676aad46c28ee1
Alif661 <alifananta253@gmail.com>
2026-03-16 05:37:21 +0700

    p

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sambungkata.alif',
-  appName: 'sambung-kata',
+  appId: 'com.alifananta.sambungkata',
+  appName: 'Sambung Kata',
   webDir: 'build'
 };
 
